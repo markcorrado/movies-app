5754b560500527eabe10eb322df8b18995629e9b
Mark Corrado <markcorrado@gmail.com>
2020-09-12 21:34:48 -0600

    starting project with ViewModel template

---
+++ app/src/main/java/com/corrado/moviesapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.corrado.moviesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.corrado.moviesapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
